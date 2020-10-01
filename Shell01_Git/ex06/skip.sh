97b9fded6a103c216b23125c128de3fe3b5b13d6
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2020-10-01 17:49:31 +0200

    Grosse modif

--- Shell01_Git/ex06/skip.sh
+++
@@ -1,2 +0,0 @@
-# Use sed 
-ls -l | sed 'n;d'
--- Shell01_Git/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | cut -f 1 -d ':' | grep -v ^# | sed -n 'n;p' | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr -s '\n' "," | sed 's/,/, /g'| rev | sed 's/,/./' | rev | sed 's/.$//'
